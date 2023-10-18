77141ec535ec5ded77faed3a12a518166020a229
Tom-Dev1 <truongthanhh88@gmail.com>
2023-10-19 00:28:50 +0700

    Repository

---
+++ BirdSellingAPI/3. Repository/Repository/BirdCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BirdSellingAPI._3._Repository.BaseRepository;
+using BirdSellingAPI._3._Repository.Data;
+
+namespace BirdSellingAPI._3._Repository.Repository
+{
+    public interface IBirdCategoryRepository : IRepositoryBase<BirdCategoryEntity>
+    {
+
+    }
+
+    public class BirdCategoryRepository : RepositoryBase<BirdCategoryEntity>, IBirdCategoryRepository
+    {
+
+    }
+}
